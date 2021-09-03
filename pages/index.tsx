4633bb0f73437e519adb070652fd9b2d3aa393eb
Fernando Gunther <lfernando.gunther@gmail.com>
2021-09-03 09:53:50 -0300

    change to typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,59 +1,57 @@
 import Head from 'next/head'
-import Link from 'next/link'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
 import Date from '../components/date'
+import { GetStaticProps } from 'next'
 
-export async function getStaticProps() {
-    const allPostsData = getSortedPostsData()
-    return {
-        props: {
-            allPostsData
-        }
-    }
-}
-
-// Here’s the starter code for getServerSideProps.
-// It’s not necessary for our blog example, so we won’t be implementing it.
-// export async function getServerSideProps(context) {
-//     return {
-//         props: {
-//         // props for your component
-//         }
-//     }
-// }
-
-export default function Home({ allPostsData }) {
+export default function Home({
+  allPostsData
+}: {
+  allPostsData: {
+    date: string
+    title: string
+    id: string
+  }[]
+}) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm Fernando Gunther. I'm software developer and love programming.</p>
+        <p>[Your Self Introduction]</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
+          (This is a sample website - you’ll be building a site like this in{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
-
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-                <Link href={`/posts/${id}`}>
+              <Link href={`/posts/${id}`}>
                 <a>{title}</a>
-                </Link>
-                <br />
-                <small className={utilStyles.lightText}>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
                 <Date dateString={date} />
-                </small>
+              </small>
             </li>
           ))}
         </ul>
       </section>
     </Layout>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
